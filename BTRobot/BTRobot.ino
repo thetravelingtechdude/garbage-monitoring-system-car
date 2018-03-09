5dc8198d52da1dcd92f706c2b5468189836b781e
thetravelingtechdude <32135728+thetravelingtechdude@users.noreply.github.com>
2018-03-09 15:32:51 +0530

    Add files via upload

---
+++ BTRobot/BTRobot.ino
@@ -0,0 +1,104 @@
+// Robot wheel Controls(M1, M2)
+#define m1 2
+#define m2 3
+#define m3 4
+#define m4 5
+
+// Robot Arm Control(M3, M4)
+#define m5 6
+#define m6 7
+#define m7 8
+#define m8 9
+
+char data = 0;//Variable for storing received data
+
+void setup()
+{
+    Serial.begin(9600);   //Sets the baud rate for serial data transmission                               
+    pinMode(2, OUTPUT);  
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+
+    // ARM control Pins
+    pinMode(6, OUTPUT);  
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+   if(Serial.available() > 0)      // Send data only when you receive data:
+   {
+      data = Serial.read();        //Read the incoming data & store into data
+      Serial.print(data);          //Print Value inside data in Serial monitor
+      Serial.print("\n");        
+      if(data == '1')
+      {              
+         digitalWrite(2, HIGH);   // Run Forward
+         digitalWrite(3, LOW);
+         digitalWrite(4, HIGH);   
+         digitalWrite(5, LOW);
+      }      
+      else if(data == '2')         //  Checks whether value of data is equal to 0
+      {
+         digitalWrite(2, LOW);   // Run Reverse
+         digitalWrite(3, HIGH);
+         digitalWrite(4, LOW);   
+         digitalWrite(5, HIGH);
+      }
+         else if(data == '3')
+         {
+         digitalWrite(2, HIGH);   // Right Turn
+         digitalWrite(3, LOW);
+         digitalWrite(4, HIGH);   
+         digitalWrite(5, HIGH);
+         }
+         else if(data == '4')
+         {
+         digitalWrite(2, HIGH);   // Left Turn
+         digitalWrite(3, HIGH);
+         digitalWrite(4, HIGH);   
+         digitalWrite(5, LOW);
+         }
+         else if (data == '5')
+         {
+         digitalWrite(6, HIGH);   // UP
+         digitalWrite(7, LOW);
+         digitalWrite(8, HIGH);    
+         digitalWrite(9, HIGH);     
+         }
+          else if(data == '6')
+         {
+         digitalWrite(6, LOW);   // DOWN
+         digitalWrite(7, HIGH);
+         digitalWrite(8, HIGH);   
+         digitalWrite(9, HIGH);       
+         }
+         else if(data == '7')
+         {
+         digitalWrite(6, HIGH);   // OPEN
+         digitalWrite(7, HIGH);
+         digitalWrite(8, HIGH);   
+         digitalWrite(9, LOW);
+         }
+         else if(data == '8')
+         {
+         digitalWrite(6, HIGH);   // CLOSE
+         digitalWrite(7, HIGH);
+         digitalWrite(8, LOW);   
+         digitalWrite(9, HIGH);
+         }
+         else if(data == '0'){
+         digitalWrite(2, HIGH);   // STOP
+         digitalWrite(3, HIGH);
+         digitalWrite(4, HIGH);   
+         digitalWrite(5, HIGH);
+         digitalWrite(6, HIGH);  
+         digitalWrite(7, HIGH);
+         digitalWrite(8, HIGH);   
+         digitalWrite(9, HIGH);
+          }
+   }
+}
---
+++ dustbin1/dustbin1.ino
@@ -0,0 +1,88 @@
+// We use a NodeMCU controller to monitor Dustbin 1
+
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
+#define BLYNK_PRINT Serial
+#define TRIGGERPIN D1
+#define ECHOPIN    D2
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon), copy and paste the auth token from the Blynk app here
+char auth[] = "";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+// Type in your Wifi network id and password
+char ssid[] = "";
+char pass[] = "";
+
+WidgetLED led(V0);
+WidgetLCD lcd(V1);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //1Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+
+  lcd.print(4,0,"Dustbin 1"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+  delay(1000);
+  lcd.clear(); //Use it to clear the LCD Widget
+  lcd.print(0, 0, "Distance in cm"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+  
+  // Please use timed events when LCD printintg in void loop to avoid sending too many commands
+  // It will cause a FLOOD Error, and connection will be dropped
+}
+
+void loop()
+{
+  lcd.clear();
+  lcd.print(0, 0, "Distance in %"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+  
+  long distance,duration;
+  
+  digitalWrite(TRIGGERPIN, LOW);  
+  delayMicroseconds(3);  
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(12); 
+  digitalWrite(TRIGGERPIN, LOW);
+
+  duration = pulseIn(ECHOPIN, HIGH);
+  
+  //distance= duration*0.034/2;
+  distance= duration/58.2;
+  Serial.print(distance);
+  Serial.println("%");
+  1:if(distance <=10)
+  {
+    lcd.clear();
+    lcd.print(0,0,"Dustbin1 Empty %");
+    lcd.print(7, 1,distance%100*10-20);
+    if(distance <= 3)
+    {
+      led.on();
+      Blynk.setProperty(V0,"color","#e80606");  // Red
+    }
+    else
+    {
+      led.on();
+      Blynk.setProperty(V0,"color","#d69e04");  // Orange
+    }
+  }
+  else if(distance <=0  || distance > 10)
+  {
+    lcd.print(0,1,"Dustbin1 Empty");
+    led.on();
+    Blynk.setProperty(V0,"color","#0c990c");  // Green
+      if (distance<=0)
+        goto l;
+  }
+  Blynk.run();
+  delay(3500);
+} 
